52270934cb73da0fe9fb94263d5d91560b91f6b9
heyfuturejesse <jesse@heyfuturejesse.com>
2020-06-19 17:46:44 +1000

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Jesse Russell on 19/6/20.
+//  Copyright © 2020 Jesse Russell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
